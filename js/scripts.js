6c32887237a5e51be981d19c08d68b2269839f04
Mathews Alufwani <mathewalufwani@gmail.com>
2020-04-12 19:33:10 +0300

    add functionality to what we do design section

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$("#submit").click(function(){
+
+		var name = $("#contactName").val();
+		var email = $("#contactEmail").val();
+		var info = $("#textArea").val();
+
+		$(".contactName").text(name);
+		$(".contactEmail").text(email);
+		$(".textArea").text(info);
+
+		$("#output").show();
+		alert("Thank you " + name + " for your feedback. We will be in touch via " +email);
+	
+		event.preventDefault();
+	});
+});
+
+$(document).ready(function() {
+  $("#shift1").click(function() {
+    $(".txt1").toggle();
+    $(".img1").toggle();
+  });
+});
+
